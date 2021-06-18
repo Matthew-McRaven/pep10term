d98ff5b094a57aedb3f75c0ecbc8268d4371451e
Matthew McRaven <mkm302@georgetown.edu>
2021-06-18 13:58:11 -0700

    Hello, terminal!

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello, terminal!" << std::endl;
+	return 0;
+}
---
+++ test/dummy.cpp
@@ -0,0 +1,5 @@
+#include "catch.hpp"
+
+TEST_CASE( "Assert that unit testing framework is installed.") {
+	REQUIRE(true);
+}
