0896336a84e7d1a652f031d06208b7b1bf55ce2a
Matthew McRaven <mkm302@georgetown.edu>
2021-06-22 19:45:50 +0000

    Add build script.
    Oops, didn't mean to ignore my build script.

---
+++ build-linux.sh
@@ -0,0 +1,11 @@
+
+mkdir -p build
+cd build
+
+(
+	source /emsdk/emsdk_env.sh
+	mkdir -p linux
+	cd linux
+	cmake ../.. -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_BUILD_TYPE=Debug
+	make -j$(nproc) && make test
+)
